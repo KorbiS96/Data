81450484e3d5425379bc8d46bbe882651f66cf6f
KorbiS96 <126156705+KorbiS96@users.noreply.github.com>
2023-04-24 17:55:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from dash import Dash, html
 from source.components.layout import create_layout
 import dash_bootstrap_components as dbc
 
-def main () -> None:
+def main():
     app = Dash(__name__)
     server = app.server
     app.title = "GFCC Database"
     app.layout = create_layout(app)
-    app.run()
+    server.run()
 
 if __name__ == "__main__":
     main() 
